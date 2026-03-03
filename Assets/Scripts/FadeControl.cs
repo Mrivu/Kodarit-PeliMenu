f2dccd507d381112efc4355185e40d18c0a4330c
Mrivu <iivari2006@outlook.com>
2026-03-03 18:12:41 +0200

    add faderoutine

--- Assets/Scripts/FadeControl.cs
+++ Assets/Scripts/FadeControl.cs
@@ -1,28 +1,57 @@
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System;
 
 public class FadeControl : MonoBehaviour
 {
-
     public float fadeDuration = 1f;
-
-    private Image fadeObject;
+    private Image fadeObject;   
     private Color originalColor;
+
+    private Coroutine coroutine;
 
     void Awake()
     {
         fadeObject = GetComponent<Image>();
+        if (fadeObject == null)
+        {
+            Debug.Log("Fade object not found");
+            return;
+        }
+
+        originalColor = fadeObject.color;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private void StartFade(float targetAlpha)
     {
-        
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+        coroutine = StartCoroutine(FadeRoutine(targetAlpha));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FadeRoutine(float targetAlpha)
     {
-        
+        float time = 0f;
+        float startAlpha  = fadeObject.color.a;
+
+        while (time < fadeDuration)
+        {
+            time += Time.deltaTime;
+
+            Color colorChange = fadeObject.color;
+            float t = time / fadeDuration;
+            colorChange.a = Mathf.Lerp(startAlpha, targetAlpha, t);
+            fadeObject.color = colorChange;
+
+            yield return null;
+        }
+
+        Color final = fadeObject.color;
+        final.a = targetAlpha;
+        fadeObject.color = final;
     }
+
 }
